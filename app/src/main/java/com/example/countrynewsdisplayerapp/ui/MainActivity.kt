8f16c201014c5a8da6ace7779016e496aa48655e
Winston <whinstondj@gmail.com>
2021-02-14 20:47:46 -0500

    Creation of the project

---
+++ app/src/main/java/com/example/countrynewsdisplayerapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.countrynewsdisplayerapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.countrynewsdisplayerapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
