c714e0068efb32d56ed21c3d2cb32cdce2d50804
Winston <whinstondj@gmail.com>
2021-02-15 11:54:25 -0500

    f001 Menu Added

--- app/src/main/java/com/example/countrynewsdisplayerapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/countrynewsdisplayerapp/ui/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.countrynewsdisplayerapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.countrynewsdisplayerapp.R
+import com.example.countrynewsdisplayerapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    val viewModel: MainViewModel by viewModels()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        //Take the navigation controlles
+        val navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+
+        //Support top toolbar
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.newsListFragment), binding.drawerLayout)
+
+        //Custom toolbar
+        binding.mainContent.myToolBar.setupWithNavController(navController, appBarConfiguration)
+
+        //Set navigation drawer
+        binding.navView.setupWithNavController(navController)
     }
 }
